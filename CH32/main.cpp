6d4a7f7582173292ff003332a3c2e82362cfc2ed
gh_lee@visionsemicon.co.kr <gh_lee@visionsemicon.co.kr>
2024-10-05 02:13:07 +0900

    no message

---
+++ CH32/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(int argc, char** argv) 
+{
+	
+	int N;
+	cin >> N;
+	vector<int> Num(N);
+	int idx;
+	int temp;
+	
+	
+	for(int i=0; i<N; i++)
+	{
+		cin >> Num[i];
+	}
+	
+	for(int i=0; i<N-1; i++)
+	{
+		idx = i;
+		for(int j=i+1; j<N; j++)
+		{
+			if(Num[j] < Num[idx] )
+			{
+				idx = j;
+			}
+		}
+		temp = Num[i];
+		Num[i] = Num[idx];
+		Num[idx] = temp;
+	}
+	
+	for(int i=0; i<Num.size(); i++)
+	{
+		cout << Num[i] <<" ";	
+	}
+	return 0;
+}
---
+++ CH33/main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(int argc, char** argv) 
+{
+	
+	int n;
+	cin >> n;
+	vector<int> data(n);
+	int idx;
+	int temp;
+	int cnt = 1;
+	
+	for(int i=0; i<n; i++)
+	{
+		cin >> data[i];
+	}
+	
+	for(int i=0; i<n-1; i++)
+	{
+		idx = i;
+		for(int j=i+1; j<n; j++)
+		{
+			if(data[j] < data[idx])
+			{
+				idx = j;	
+			}	
+		}
+		temp = data[i];
+		data[i] = data[idx];
+		data[idx] = temp;	
+	}
+	
+	for(int i=data.size(); i>0; i--)
+	{
+		if(data[i-1] > data[i]) 
+		{
+			cnt ++;
+		}
+		
+		if(cnt == 3)
+		{
+			cout << data[i];
+			break;
+		}
+	}
+	
+	return 0;
+}
