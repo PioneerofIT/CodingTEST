f19345e6c94e5fa37231e75ae76a45e7f5cf9adc
PioneerofIT <rjsgml3736@naver.com>
2024-10-25 00:24:44 +0900

    history update

--- CH46/main.cpp
+++ CH46/main.cpp
@@ -11,52 +11,55 @@ int main(int argc, char** argv)
 	
 	int n;
 	cin >> n;
+	int pos=0, cnt=0, total=0;
+	
+	// pos : 카운트가 감소함에따라 증가하는 index Pos 변수
+	// cnt : 타임 카운트 체크
+	// total : 총 작업시간 
 	
 	vector<int> a(n+1);
 	
 	for(int i=1; i<=n; i++)
 	{
 		cin >> a[i];
+		total += a[i];
 	}
 	
 	int k;
 	cin >> k;
 	
-	int pos = 1;
-	int cnt = 0;
-	
-	
-	// k=5 start
-	while(k != 0)
+	if(k >= total) // K 가 크면 이미 모든 작업이 끝난 후 정전 
 	{
-		if(a[pos] != 0)
-		{
-			a[pos] --;
-		}
-		else
-		{
-			cnt ++;
-			pos ++;
-			a[pos] --;
-		}
-		if(pos == n)
-			pos = 1;
+		cout << "-1" << endl;
+		return 0;
+	}
+	
+	
+	// 작업 하나 당 1의 카운트가 증가한다. 
+	while(1)
+	{
+		pos++;
+		if(pos > n) pos = 1;
+		if(a[pos] == 0) continue; // 작업량이 남아 있지 않으면 그냥 건너뛴다.
 		
-		k--;	
+		// 작업량이 있는 로직
+		a[pos] --;
+		cnt ++;
+		if(cnt == k) break; 
+		 
 	}
 	
-	if(cnt == n)
+	// 정전이 끝난 후 다음 작업을 알아내는 과정
+	// 주의 사항 : 다음 작업이 a[pos] == 0  이면 출력하면 안됌 
+	while(1)
 	{
-		cout << "-1" ;
+		pos ++;
+		if(pos > n) pos = 1; // 항상 인덱스가 마지막인지 체크. 
+		if(a[pos] != 0)
+			break;
 	}
-	for(int i=1; i<=n; i++)
-	{
-		if(a[i] != 0 )
-		{
-			cout << a[i];
-			return 0;			
-		}		
-	}
+	cout << pos;
+
 
 	 
 	return 0;
